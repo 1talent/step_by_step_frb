37fdc22ae3e4020837db756edcd6ef7ebeb84cac
drunisa007 <newarunkatwal@gmail.com>
2023-02-03 07:53:09 +0000

    install permanently corrosion and use it

---
+++ windows/mrrust.cmake
@@ -0,0 +1,12 @@
+find_package(Corrosion REQUIRED)
+
+
+corrosion_import_crate(MANIFEST_PATH ../src_rust/Cargo.toml)
+
+# Flutter-specific
+
+set(CRATE_NAME "src_rust")
+
+target_link_libraries(${BINARY_NAME} PRIVATE ${CRATE_NAME})
+
+list(APPEND PLUGIN_BUNDLED_LIBRARIES $<TARGET_FILE:${CRATE_NAME}-shared>)
